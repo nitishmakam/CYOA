317a3628786ed3e0e7d2b5741094fcf0f8135284
Nitish Makam <nitishmakam@ITSupportsMBP5.bbrouter>
2022-12-06 18:32:43 +0530

    Added main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fileName := flag.String("file", "gopher.json", "JSON file containing the chapters")
+	flag.Parse()
+
+	f, err := os.Open(*fileName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	d := json.NewDecoder(f)
+
+	var story Story
+
+	if err := d.Decode(&story); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", story)
+
+}
